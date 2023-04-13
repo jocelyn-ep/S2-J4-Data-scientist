350232c50ad6d7c7b73fecfac97bfbe44aefd7cd
jocelyn-ep <jocelyn.epaillard.pro@gmail.com>
2023-04-13 14:37:30 +0200

    Premier commit avec 01_cryptocurrencies

---
+++ README.md
@@ -0,0 +1,1 @@
+THP - Projet validant - Data scientist
---
+++ lib/00_journalists.rb
@@ -0,0 +1,84 @@
+def get_handles 
+  handles = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+  return handles
+end
+
+def h_count(handles)
+  #La méthode .count appliquée à l'array handles compte le nombre d'éléments. Aurait aussi marché avec .length et .size
+  puts '-' * 10
+  puts "Il y a #{handles.count} handles dans cette liste"
+end
+
+def h_min_length(handles)
+  #Ma compréhension : La méthode .min_by crée un hash, leur assigne une valeur (ici handle.length) puis parcours chaque key (ici handle) pour trouver la valeur min
+  puts '-' * 10
+  min_handle = handles.min_by{|handle| handle.length}
+  puts "Le handle le plus court est #{min_handle}"
+end
+
+def h_length_5(handles)
+  #La méthode .count appliquée à l'array handles_short compte le nombre d'éléments selon une condition (ici que la longueur des handles soit 5)
+  puts '-' * 10
+  handles_5 = handles.count {|handle| handle.length == 6}
+  puts "Il y a #{handles_5} handles de 5 caractères (sans compter le @)"
+end
+
+def h_capital(handles)
+  #La méthode .count appliquée à l'array handles_short compte le nombre d'éléments selon une condition (ici que le handle commence par une lettre majuscule après le @)
+  puts '-' * 10
+  handles_capital = handles.count {|handle| handle =~ /^@[A-Z]/}
+  puts "Il y a #{handles_capital} handles commençant par une majuscule (sans compter le @)"
+end
+
+def h_sorted(handles)
+  #La méthode .sort seule permet de trier par ordre alphabétique
+  puts '-' * 10
+  handles_sorted_a = handles.sort
+  puts "Les 5 premiers handles triés alphabétiquement sont #{handles_sorted_a[0..4]}"
+end
+
+def h_sorted_length(handles)
+#La méthode .sort permet de trier par ordre de longueur selon une condition
+  puts '-' * 10
+  handles_sorted_length = handles.sort_by{|handle| handle.length}
+  puts "Les 5 premiers handles triés par longueurs sont #{handles_sorted_length[0..4]}"
+end
+
+def h_position_epenser(handles)
+  #La méthode .index permet d'indiquer la position de l'argument dans l'array
+  puts '-' * 10
+  puts "La position de la personne @epenser dans l'array est #{handles.index("@epenser")}"
+end
+
+def h_repartition(handles)
+  #Boucle for avec les éléments expliqués ci-dessus
+  puts '-' * 10
+  min_h = handles.min_by{|handle| handle.length}
+  max_h = handles.max_by{|handle| handle.length}
+  
+  for i in min_h.length..max_h.length do
+    repartition = handles.count {|handle| handle.length == i}
+    if repartition == 1 
+      then
+      puts "Il y a #{repartition} handle de #{i} lettres (sans compter le @)"
+    elsif
+      repartition != 0 
+      then
+      puts  "Il y a #{repartition} handles de #{i} lettres (sans compter le @)"
+    end
+  end
+end
+
+def perform
+  handles = get_handles
+  h_count(handles)
+  h_min_length(handles)
+  h_length_5(handles)
+  h_capital(handles)
+  h_sorted(handles)
+  h_sorted_length(handles)
+  h_position_epenser(handles)
+  h_repartition(handles)
+end
+
+perform
